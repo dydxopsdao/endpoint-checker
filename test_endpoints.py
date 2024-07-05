75dc5ddd60be90d7567dfb6005264218ca49322b
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-07-05 16:42:21 +0200

    Make script verbose and update README

--- test_endpoints.py
+++ test_endpoints.py
@@ -2,11 +2,20 @@ import requests
 import csv
 import json
 
-def test_endpoints(endpoints, timeout=10):  # Added timeout parameter with a default of 10 seconds
+
+def test_endpoints(
+    endpoints, timeout=10
+):  # Added timeout parameter with a default of 10 seconds
     results = []
-    for endpoint in endpoints:
+    for i, endpoint in enumerate(endpoints):
+        print(
+            f"({i + 1}/{len(endpoints)}) Testing {endpoint['name']} at {endpoint['openmetrics_endpoint']} ... ",
+            end="",
+            flush=True,
+        )
+
         try:
-            response = requests.get(endpoint['openmetrics_endpoint'], timeout=timeout)
+            response = requests.get(endpoint["openmetrics_endpoint"], timeout=timeout)
             if response.status_code == 200 and response.text:
                 status = "Good"
             else:
@@ -15,28 +24,34 @@ def test_endpoints(endpoints, timeout=10):  # Added timeout parameter with a def
             status = "Timeout"  # Specific status for timeout
         except Exception as e:
             status = f"Error: {str(e)}"  # Include error message in the status
-        
-        results.append({
-            "name": endpoint['name'],
-            "endpoint": endpoint['openmetrics_endpoint'],
-            "status": status
-        })
-    
+
+        print(status)
+
+        results.append(
+            {
+                "name": endpoint["name"],
+                "endpoint": endpoint["openmetrics_endpoint"],
+                "status": status,
+            }
+        )
+
     return results
 
+
 def write_results_to_csv(results, filename):
-    with open(filename, mode='w', newline='') as file:
-        fieldnames = ['name', 'endpoint', 'status']
+    with open(filename, mode="w", newline="") as file:
+        fieldnames = ["name", "endpoint", "status"]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        
+
         writer.writeheader()
         for result in results:
             writer.writerow(result)
+
 
 if __name__ == "__main__":
     # Read endpoints from JSON file
     with open("endpoints.json", "r") as file:
         endpoints = json.load(file)
-    
+
     results = test_endpoints(endpoints)
     write_results_to_csv(results, "endpoint_status.csv")
